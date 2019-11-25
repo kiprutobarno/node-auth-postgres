4c2b6d56aebcd8c5395c8127e171063595d22bdb
ywalakamar <barxwells@gmail.com>
2019-11-25 19:48:11 +0300

    modified

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({ connectionString: process.env.DB_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
--- manageDB.js
+++ manageDB.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL
+  connectionString: process.env.DATABASE_URL
 });
 
 pool.on("connect", () => {
   console.log("....connected to database....");
 });
-  
+
 /** Create users table */
 const createUsersTable = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS users(
